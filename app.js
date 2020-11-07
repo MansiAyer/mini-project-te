319768d4c0906e68fdaa356a8f3bb9d063763f83
Mansi Ayer <42694815+MansiAyer@users.noreply.github.com>
2020-11-07 14:48:06 +0530

    created local hosting

---
+++ app.js
@@ -0,0 +1,55 @@
+const http = require('http')
+
+let fsm = require('fs')
+let fsd = require('fs')
+
+const portm = 3600
+const portd = 3400
+
+const serverm = http.createServer(function(req, res) {
+  res.statusCode = 220
+  res.setHeader('Content-Type', 'text/html')
+  fsm.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File m not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+  
+})
+
+const serverd = http.createServer(function(req, res) {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  fsd.readFile('wdlmini.html', function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File d not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+  
+})
+
+serverm.listen(portm, function(error) {
+	if (error) {
+	  console.log(`we have found`, error)
+	}
+	else {
+		console.log('server listens to meep' + portm)
+	}
+})
+
+serverd.listen(portd, function(error) {
+	if (error) {
+	  console.log(`we have found`, error)
+	}
+	else {
+		console.log('server listens to dia ' + portd)
+	}
+})
